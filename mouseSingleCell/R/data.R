01f722d2f7dbb785b5a7fd0f6afebafb31aa1c7e
efheuston <efheuston@gmail.com>
2021-05-17 16:12:13 -0400

    Fixed mouseIDTable but now notebook is broken

---
+++ mouseSingleCell/R/data.R
@@ -0,0 +1,17 @@
+#' mouseIDTable
+#'
+#' A list of mouse genes and ID conversions
+#'
+#' @format Table with 24117 obs. of 5 variables
+#' \describe{
+#'   \item{Mouse.Gene_ID}{Ensemble mosue gene id}
+#'   \item{MGI}{MGI ID}
+#'   \item{Entrez.Gene_ID}{Entrez gene ID}
+#'   \item{Human.Gene_ID}{Human homologue}
+#'   \item{HGNC}{HGNC ID}
+#' }
+#' @concept data
+#' @source biomaRt
+
+#'
+"mouseIDTable"
