2155696840806387950e6f5e68e3361c04a34895
efheuston <efheuston@gmail.com>
2021-05-21 15:55:51 -0400

    Repaired multiple functions and updated EdgeR analysis

--- mouseSingleCell/data-raw/data.R
+++ mouseSingleCell/data-raw/data.R
@@ -1,6 +1,18 @@
 # data-raw/data.R
 # Import data lists
 
+#' @title mouseIDTable
+#' @description Local table containing mouse gene identifier conversion information. Table generated 2021.04.20 with bioconductor biomaRt
+#' @format A data frame with 24117 rows and 5 columns
+#' \describe{
+#' \item{\code{Mouse.Gene_ID}}{double mouse ensembl gene ID}
+#' \item{\code{MGI}}{double MGI gene ID}
+#' \item{\code{Entrez.Gene_ID}}{double entrez gene ID}
+#' \item{\code{Human.Gene_ID}}{double human homologue ensembl gene ID}
+#' \item{\code{HGNC}}{double HGNC gene ID}
+#' }
+#' @source \url{http://www.ensembl.org}
+
 # retrieve mouseIDTable path
 mouseIDTable.file <- system.file(
 	"extdata",
